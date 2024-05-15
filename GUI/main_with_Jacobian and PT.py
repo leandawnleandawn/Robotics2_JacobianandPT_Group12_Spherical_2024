f3942bf936db5509c0ac707ca96d7825d1f061e3
leandawnleandawn <lyndonallensales@gmail.com>
2024-05-15 21:52:46 +0800

    Added Path and Trajectory planning on GUI

--- GUI/main_with_Jacobian and PT.py
+++ GUI/main_with_Jacobian and PT.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import numpy as np
+import roboticstoolbox as rtb
 from roboticstoolbox import SerialLink, RevoluteDH, PrismaticDH
 
 class RoboticProgram(tk.Tk):
@@ -10,13 +11,14 @@ class RoboticProgram(tk.Tk):
         self.windowTitle = tk.Label(text="Kinematic Analysis Calculator")
         self.windowTitle.pack()
         
-        FKin = tk.Button(text = "Forward Kinematics", command = FkinWindow)
+        FKin = tk.Button(text = "Forward Kinematics and Jacobian", command = FkinWindow)
         FKin.pack()
         
         IKin = tk.Button(text = "Inverse Kinematics", command= IkinWindow)
         IKin.pack()
         
-        PT = tk.Button(text = "Path and Trajectory Planning")
+        PT = tk.Button(text = "Path and Trajectory Planning", command = PathandTrajWindow)
+        PT.pack()
         
 class Window(RoboticProgram):
     def __init__(self):
@@ -426,7 +428,121 @@ class IkinWindow(Window):
         d3 = (np.sqrt((r**2) + (s**2)) - a2 - a3) * 100
         return theta1, theta2, d3
         
-class PathandTrajWindow((()))
+class PathandTrajWindow(RoboticProgram):
+    def __init__(self):
+        
+        self.windowTitle = tk.Toplevel(master = robot)
+        
+        PT = tk.LabelFrame(master=self.windowTitle, text = "Program Traj Functions", font=(5))
+        PT.grid(row = 0, column = 0)
+        
+        pick_place = tk.Button(PT, text = "Pick and Place", command= self.pickPlace)
+        pick_place.grid(row=0, column=0)
+        Welding = tk.Button(PT, text = "Welding" , command=self.welding)
+        Welding.grid(row=1, column=0)
+        
+    def pickPlace(self):
+        
+        a1 = 20
+        a2 = 15
+        a3 = 15
+        
+        H01 = RevoluteDH(a1, 0, np.pi/2, 0, [-np.pi/2, np.pi/2])
+        H12 = RevoluteDH(0, 0, np.pi/2, np.pi/2, [-np.pi/2, np.pi/2])
+        H23 = PrismaticDH(0,0,0,a2+a3,[0, 0.05])
+        
+        sphericalManipulator = SerialLink([H01, H12, H23])
+        
+        print(sphericalManipulator)
+        
+        conveyor_beltT1, conveyor_beltT2, conveyor_beltd3 = self.invKins(a1, a2, a3,  10, 0, 3)
+        box1T1, box1T2, box1d3 = self.invKins(a1, a2, a3, -5*np.sin(np.pi/4), -5*np.cos(np.pi/4), 2)
+        
+        conveyor_beltT1 = self.self.convert_to_radians(conveyor_beltT1)
+        conveyor_beltT2 = self.self.convert_to_radians(conveyor_beltT2)
+        box1T1 = self.self.convert_to_radians(box1T1)
+        box1T2 = self.self.convert_to_radians(box1T2)
+
+        q0 = np.array([0, 0, 0])
+        q1 = np.array([conveyor_beltT1, conveyor_beltT2, conveyor_beltd3])
+        q2 = np.array([box1T1, box1T2, box1d3])
+
+        t = np.linspace(0, 20, num = 50)
+        traj1 = rtb.jtraj(q0, q1, t)
+        traj2 = rtb.jtraj(q1, q0, t)
+        traj3 = rtb.jtraj(q0, q2, t)
+        traj4 = rtb.jtraj(q2, q0, t)
+        
+        while True:
+            sphericalManipulator.plot(traj1.s, limits = [-5, 30, -10, 10, 0, 30])
+            sphericalManipulator.plot(traj2.s, limits = [-5, 30, -10, 10, 0, 30])
+            sphericalManipulator.plot(traj3.s, limits = [-5, 30, -10, 10, 0, 30])
+            sphericalManipulator.plot(traj4.s, limits = [-5, 30, -10, 10, 0, 30])
+    
+    def welding(self):
+        a1 = 20
+        a2 = 15
+        a3 = 15
+        
+        H01 = RevoluteDH(a1, 0, np.pi/2, 0, [-np.pi/2, np.pi/2])
+        H12 = RevoluteDH(0, 0, np.pi/2, np.pi/2, [-np.pi/2, np.pi/2])
+        H23 = PrismaticDH(0,0,0,a2+a3,[0, 0.05])
+        
+        sphericalManipulator = SerialLink([H01, H12, H23])
+        
+        print(sphericalManipulator)
+        
+        rectanglep1T1, rectanglep1T2, rectanglep1d3 = self.invKins(a1, a2, a3, -5, 5, 3)
+        rectanglep2T1, rectanglep2T2, rectanglep2d3 = self.invKins(a1, a2, a3,  5, 5, 3)
+        rectanglep3T1, rectanglep3T2, rectanglep3d3 = self.invKins(a1, a2, a3, 10, 5, 3)
+        rectanglep4T1, rectanglep4T2, rectanglep4d3 = self.invKins(a1, a2, a3, 10, -5, 3)
+        
+        rectanglep1T1 = self.convert_to_radians(rectanglep1T1)
+        rectanglep1T2 = self.convert_to_radians(rectanglep1T2)
+        rectanglep2T1 = self.convert_to_radians(rectanglep2T1)
+        rectanglep2T2 = self.convert_to_radians(rectanglep2T2)
+        rectanglep3T1 = self.convert_to_radians(rectanglep3T1)
+        rectanglep3T2 = self.convert_to_radians(rectanglep3T2)
+        rectanglep4T1 = self.convert_to_radians(rectanglep4T1)
+        rectanglep4T2 = self.convert_to_radians(rectanglep4T2)
+        
+        q0 = np.array([0, 0, 0])
+        q1 = np.array([rectanglep1T1, rectanglep1T2, rectanglep1d3])
+        q2 = np.array([rectanglep2T1, rectanglep2T2, rectanglep2d3])
+        q3 = np.array([rectanglep3T1, rectanglep3T2, rectanglep3d3])
+        q4 = np.array([rectanglep4T1, rectanglep4T2, rectanglep4d3])
+
+
+        t = np.linspace(0, 20, num = 50)
+        traj1 = rtb.jtraj(q0, q1, t)
+        traj2 = rtb.jtraj(q1, q2, t)
+        traj3 = rtb.jtraj(q2, q3, t)
+        traj4 = rtb.jtraj(q3, q4, t)
+        traj5 = rtb.jtraj(q4, q1, t)
+        traj6 = rtb.jtraj(q1, q0, t)
+
+        print(traj1)
+
+        while True:
+            sphericalManipulator.plot(traj1.s, limits = [-5, 30, -10, 10, 0, 30])
+            sphericalManipulator.plot(traj2.s, limits = [-5, 30, -10, 10, 0, 30])
+            sphericalManipulator.plot(traj3.s, limits = [-5, 30, -10, 10, 0, 30])
+            sphericalManipulator.plot(traj4.s, limits = [-5, 30, -10, 10, 0, 30])
+            sphericalManipulator.plot(traj5.s, limits = [-5, 30, -10, 10, 0, 30])
+            sphericalManipulator.plot(traj6.s, limits = [-5, 30, -10, 10, 0, 30])
+            
+    def invKins(self, a1, a2, a3,  x_03, y_03, z_03):
+        s = z_03 - a1
+        r = np.sqrt((x_03**2) + (y_03**2))
+        theta1 = np.arctan(y_03/x_03) * 180/np.pi
+        theta2 = np.arctan(s/r) * 180/np.pi
+        d3 = np.sqrt((r**2) + (s**2)) - a2 - a3
+        return theta1, theta2, d3
+    
+    def convert_to_radians(self, deg):
+        return deg * (np.pi/180)
+    
+    
 robot = RoboticProgram()
 robot.mainloop()
 
