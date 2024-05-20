dd5374dbfaf819f0c677b6bdc424b2afe87a3235
leandawnleandawn <lyndonallensales@gmail.com>
2024-05-20 23:17:07 +0800

    Added MATLAB Files for Jacobian and PT

---
+++ Path and Trajectory/Pick_and_Place_Step2.m
@@ -0,0 +1,6 @@
+% Run this code to plot the Pick and Place Robot Program
+
+plot(Spherical, traj1)
+plot(Spherical, traj2)
+plot(Spherical, traj3)
+plot(Spherical, traj4)
---
+++ Path and Trajectory/Welding_Step2.m
@@ -0,0 +1,8 @@
+% Run this code to plot the Pick and Place Robot Program
+
+plot(Spherical, traj1)
+plot(Spherical, traj2)
+plot(Spherical, traj3)
+plot(Spherical, traj4)
+plot(Spherical, traj5)
+plot(Spherical, traj6)
